c2e1a457a381a5996dc37be138e019912f0bb50b
christiannparak <christiannparak47@gmail.com>
2024-12-30 18:23:17 +0545

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-//adding dynamic animation to the role
-const roles = ["Computer Science Student", "Web Designer", "Programmer"];
-let index=0;
-
-setInterval(()=> {
- 
-  document.querySelector('.role').textContent = roles[index];
-  index = (index + 1) % roles.length;
-},3000);
